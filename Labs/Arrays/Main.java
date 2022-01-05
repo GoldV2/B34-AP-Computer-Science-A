a83fdef4a4a8a7f63a1dd1706382c27e534e45eb
Rafael Almeida <rafaelpbcp@gmail.com>
2022-01-05 11:18:52 -0500

    Changed the input format to see count and increment by multiple

--- Labs/Arrays/Main.java
+++ Labs/Arrays/Main.java
@@ -8,9 +8,12 @@ public class Main {
         int size = Integer.parseInt(input("Enter size of array"));
         ArrayItem[] items = populate(size);
         
-        System.out.println("-1: Quit\n\"index\" to increment a count\n\"index.INT\" to multiply the count by INT\"\n\"index.123\" to see index count");
+        System.out.println("-1: Quit\n\"index\" to increment a count\n\"index INT\" to multiply the count by INT\"\n\"index count\" to see index count");
         while (true) {
-            double ind = Double.parseDouble(input(""));
+            String input = input("");
+            int ind = get_index(input);
+            String command = get_command(input);
+
             if (ind < -1 || ind >= size) {
                 System.out.println("Invalid Input, try again!");
             }
@@ -18,13 +21,16 @@ public class Main {
                 display(items);
                 System.out.println("Goodbye!!");
             }
-            else if ((int)((ind-(int)ind)*1000) == 123 || (int)((ind-(int)ind)*1000) == 122) {
+            else if (command.equals("count")) {
                 System.out.print(items[(int)ind].getCount());
             }
             else {
-                int remainder = (int)(ind % 1 * 10);
-                int indInt = (int)ind;
-                items[indInt].increment(remainder);
+                if (command.equals("")) {
+                    items[ind].increment(0);
+                }
+                else {
+                    items[ind].increment(Integer.parseInt(command));
+                }
             }
         }
 
@@ -37,6 +43,24 @@ public class Main {
         }
 
         return items;
+    }
+
+    public static int get_index(String s) {
+        if (s.contains(" ")) {
+            return Integer.parseInt(s.substring(0, s.indexOf(" ")));
+        }
+        else {
+            return Integer.parseInt(s);
+        }
+    }
+
+    public static String get_command(String s) {
+        if (s.contains(" ")) {
+            return s.substring(s.indexOf(" ")+1);
+        }
+        else {
+            return "";
+        }
     }
 
     public static String input(String s) {
